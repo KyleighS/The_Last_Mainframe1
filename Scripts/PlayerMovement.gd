e42f4f6ff098a60771b83495383922333dc657be
KyleighS <111814352+KyleighS@users.noreply.github.com>
2025-10-06 01:51:39 -0400

    full health system(damage, healing, checkpoints, immunity))

--- Scripts/PlayerMovement.gd
+++ Scripts/PlayerMovement.gd
@@ -13,6 +13,14 @@ const JUMP_VELOCITY = -300.0
 # Gets the input direction
 var direction = Input.get_axis("Move_Left", "Move_Right")
 
+#Health System
+var max_health = 3
+var _health = max_health
+@onready var respawn_timer = $RespawnTimer
+@onready var imunity_timer = $ImunityTimer
+var can_damage = true
+
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
@@ -47,6 +55,7 @@ func _physics_process(delta: float) -> void:
 		animated_sprite.play("Jump")
 	shoot(animated_sprite.flip_h)
 	move_and_slide()
+	heal()
 	
 func shoot(dir):
 	if Input.is_action_just_pressed("Shoot"):
@@ -54,3 +63,44 @@ func shoot(dir):
 		bulletObj.init(dir)
 		get_parent().add_child(bulletObj)
 		bulletObj.global_position = $Marker2D.global_position
+		
+
+#Health Functions
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	#checks if it was an enemy and if the player can take damage
+	if area.is_in_group("Enemy") && can_damage:
+		#subtracst health from player
+		_health -= 1
+		get_node("Camera2D/HealthBar").get_child(_health).queue_free()
+		print("player damaged ", _health)
+		#starts imunity
+		can_damage = false
+		print("imunity starts")
+		imunity_timer.start()
+		
+		#kills player
+		if _health <= 0:
+			Engine.time_scale = 0.5
+			respawn_timer.start()
+			get_node("CollisionShape2D").queue_free()
+			#get_tree().reload_current_scene()
+			print("Player Killed")
+
+#for respawn timer
+func _on_respawn_timer_timeout():
+	Engine.time_scale = 1.0
+	get_tree().reload_current_scene()
+
+#for imunity timer, allows player to take damage again
+func _on_imunity_timer_timeout():
+	can_damage = true
+	print("imunity ends")
+
+func heal():
+	if Input.is_action_just_pressed("Heal"):
+		_health += 1
+		#aget_node("Camera2D/HealthBar")a.add_child(heart)
+		if _health > max_health:
+			_health = max_health
+			
+		print("player healed to ", _health)
--- Scripts/kill_zone.gd
+++ Scripts/kill_zone.gd
@@ -1,13 +1,18 @@
 extends Area2D
 
 @onready var timer = $Timer
+var checkpoint_manager
+var player
+
+func _ready() -> void:
+	checkpoint_manager = get_parent().get_node("CheckpointManager")
+	player = get_parent().get_node("Player")
 
 func _on_body_entered(body):
-	print("You Died")
+	#get_tree().reload_current_scene()
 	Engine.time_scale = 0.5
-	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
 
 func _on_timer_timeout():
 	Engine.time_scale = 1.0
-	get_tree().reload_current_scene()
+	player.position = checkpoint_manager.last_location
