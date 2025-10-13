7a43c643a33e8d748a63786e093a40be8e56636d
KyleighS <111814352+KyleighS@users.noreply.github.com>
2025-10-13 01:05:10 -0400

    Drag captcha is working

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 func _on_play_pressed() -> void:
 	$"SFX Click".play()
-	get_tree().change_scene_to_file("res://Scenes/TestingScenes/TestingScene.tscn")
+	get_tree().change_scene_to_file("res://Scenes/BasicGame/BasicMovement.tscn")
 
 func _on_quit_pressed() -> void:
 	$"SFX Click".play()
