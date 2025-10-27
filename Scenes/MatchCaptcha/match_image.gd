3669af51a1db8ee134ce57b94aa74b316abc7680
ltu-lpham <142945673+ltu-lpham@users.noreply.github.com>
2025-10-27 11:02:33 -0400

    Added error feedback for the Match Captcha minigame and randomized left/right images with no initial match.

--- Scenes/MatchCaptcha/match_image.gd
+++ Scenes/MatchCaptcha/match_image.gd
@@ -9,6 +9,10 @@ extends Node2D
 @onready var left_arrow = $LeftArrow
 @onready var right_arrow = $RightArrow
 @onready var match_verify_button = $MatchVerifyButton
+@onready var error_label = $ErrorLabel
+@onready var error_overlay = $ErrorOverlay
+@onready var camera = $Camera2D
+@onready var flash_timer = $FlashTimer
 
 @onready var sfx_hover: AudioStreamPlayer = $"../SFX Hover"
 @onready var sfx_click: AudioStreamPlayer = $"../SFX Click"
@@ -18,6 +22,11 @@ extends Node2D
 var Door = preload("res://Scenes/BasicGame/door.tscn").get_script()
 
 var current_index := 0
+var original_camera_pos: Vector2
+var shake_amount = 10
+var shake_duration = 0.2
+var shake_elapsed = 0.0
+var shaking = false
 
 func _ready():
 	# Example: preload images
@@ -29,6 +38,25 @@ func _ready():
 		preload("res://Assets/images/match_captcha/right4.jpg"),
 		preload("res://Assets/images/match_captcha/right5.jpg")
 	]
+	
+	#Generate random image sequence
+	randomize()
+	
+	# Shuffle the right images
+	right_image_textures.shuffle()
+	current_index = 0
+	
+	# Pick a random left image, making sure it doesn't match the first right image
+	left_image_texture = right_image_textures[randi() % right_image_textures.size()]
+	while left_image_texture == right_image_textures[current_index]:
+		left_image_texture = right_image_textures[randi() % right_image_textures.size()]
+	
+	error_label.visible = false
+	error_overlay.visible = false
+	original_camera_pos = camera.position
+	flash_timer.one_shot = true
+	flash_timer.wait_time = 0.4
+	flash_timer.timeout.connect(reset_flash)
 
 	# Assign to the UI
 	left_image.texture = left_image_texture
@@ -60,10 +88,40 @@ func _on_match_verify_button_pressed():
 	else:
 		print("Fail! Resetting scene.")
 		sfx_failure.play()
+		show_error_feedback()
 		reset_captcha()
+
+func _process(delta):
+	if shaking:
+		if shake_elapsed < shake_duration:
+			var offset = Vector2(randf_range(-shake_amount, shake_amount), randf_range(-shake_amount, shake_amount))
+			camera.position = original_camera_pos + offset
+			shake_elapsed += delta
+		else:
+			camera.position = original_camera_pos
+			shaking = false
+			shake_elapsed = 0.0
 
 # --- Reset function ---
 func reset_captcha():
 	current_index = 0
 	right_image.texture = right_image_textures[current_index]
 	left_image.texture = left_image_texture
+	
+# Show visual feedback
+func show_error_feedback():
+	error_label.text = "ERROR"
+	error_label.visible = true
+	error_overlay.visible = true
+	flash_timer.start()
+	start_screen_shake()
+	
+# Reset after flash
+func reset_flash():
+	error_overlay.visible = false
+	error_label.visible = false
+	
+# Simple screen shake
+func start_screen_shake():
+	shaking = true
+	shake_elapsed = 0.0
