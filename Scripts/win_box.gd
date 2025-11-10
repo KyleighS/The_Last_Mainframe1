07ee8267074085dc326a3d587c5f67cc25849c9f
KyleighS <111814352+KyleighS@users.noreply.github.com>
2025-11-10 11:20:05 -0500

    all captcha have doors in level and cleaned up some folders

--- Scripts/win_box.gd
+++ Scripts/win_box.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
-
 func _on_body_entered(body: Node2D) -> void:
+	print(body)
 	if body.is_in_group("Player"):
-		body.get_node("CameraPoint/WinScreen").show()
+		body.get_node("CameraPoint/Camera2D/WinScreen").show()
 		Engine.time_scale = 0
